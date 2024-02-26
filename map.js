efacda9a37a1b751b30fe0948260975f6854aa36
isantiagojaramillo <sajaramillogr@cesde.net>
2024-02-26 14:57:35 -0500

    JS-METHODS: map

--- map.js
+++ map.js
@@ -1,8 +1,32 @@
 import { students } from "./data/data.js";
 
-const fullnames = students.map(function (student) {
-    // return student;
-    return student.name + ' ' + student.lastname;
-})
+// const fullnames = students.map(function (student) {
+//     // return student;
+//     return student.name + ' ' + student.lastname;
+// })
 
-console.log(fullnames);
+// console.log(fullnames);
+
+// const names = students.map(student => student.name + " " + student.lastname);
+
+// console.log(names);
+
+// const names = students.map(student => {
+//     return {
+//         // fullname: student.name + " " + student.lastname,
+//         // age: student.age,
+//         // course: student.course,
+//         ...student,
+//         title: `${student.name} - ${student.course}`
+//     }
+// });
+
+// console.log(names);
+
+const doubleAge = students.map(student => ({
+    ...student,
+    title: `${student.name} - ${student.course}`
+}))
+.map(student => ({...student, age: student.age * 2}));
+
+console.log(doubleAge);
